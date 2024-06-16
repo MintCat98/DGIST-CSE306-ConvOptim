224f661da45bbfebe9a84db2cf562ff1fd32f415
MintCat <presidentmc9898@gmail.com>
2024-06-16 16:34:06 +0900

    [feat] ver.1

--- hw2/hw2.c
+++ hw2/hw2.c
@@ -13,37 +13,43 @@
 
 // This implementation is simply copied from "main.c".
 // Your job is to modify and optimize it for better performance!
+static inline Pixel convolution( // make this function as an inline
+    Pixel* input, int x, int y, int width, int height, float* filter) {
+    double r = 0, g = 0, b = 0;
 
-static Pixel convolution(
-        Pixel* input, int x, int y, int width, int height, float* filter) {
-    double r = 0;
-    double g = 0;
-    double b = 0;
+    int x_start = (x - 1 < 0) ? 0 : x - 1;
+    int x_end = (x + 1 >= width) ? width - 1 : x + 1;
+    int y_start = (y - 1 < 0) ? 0 : y - 1;
+    int y_end = (y + 1 >= height) ? height - 1 : y + 1;
 
-    for (int dx = -1; dx < 2; ++dx) {
-        for (int dy = -1; dy < 2; ++dy) {
-            if ((y + dy) < 0 || (y + dy) >= height)
-                continue;
+    // Loop unrolling
+    for (int dy = y_start; dy <= y_end; ++dy) {
+        for (int dx = x_start; dx <= x_end; dx += 2) {
+            int filter_x1 = dx - x + 1;
+            int filter_y1 = dy - y + 1;
+            float filter_value1 = filter[filter_x1 + filter_y1 * 3];
 
-            if ((x + dx) < 0 || (x + dx) >= width)
-                continue;
+            r += input[dx + dy * width].r * filter_value1;
+            g += input[dx + dy * width].g * filter_value1;
+            b += input[dx + dy * width].b * filter_value1;
 
-            r += input[(x+dx)+(y+dy)*width].r * filter[(dx+1)+(dy+1)*3];
-            g += input[(x+dx)+(y+dy)*width].g * filter[(dx+1)+(dy+1)*3];
-            b += input[(x+dx)+(y+dy)*width].b * filter[(dx+1)+(dy+1)*3];
+            if (dx + 1 <= x_end) {
+                int filter_x2 = filter_x1 + 1;
+                float filter_value2 = filter[filter_x2 + filter_y1 * 3];
+
+                r += input[(dx + 1) + dy * width].r * filter_value2;
+                g += input[(dx + 1) + dy * width].g * filter_value2;
+                b += input[(dx + 1) + dy * width].b * filter_value2;
+            }
         }
     }
 
-    if (r < 0) r = 0;
-    if (g < 0) g = 0;
-    if (b < 0) b = 0;
+    r = (r < 0) ? 0 : (r > 255) ? 255 : r;
+    g = (g < 0) ? 0 : (g > 255) ? 255 : g;
+    b = (b < 0) ? 0 : (b > 255) ? 255 : b;
 
-    if (r > 255) r = 255;
-    if (g > 255) g = 255;
-    if (b > 255) b = 255;
-    
     Pixel p;
-    memset(&p, 0, sizeof(p));
+    memset(&p, 0, sizeof(p));  // Pixel 구조체를 0으로 초기화
 
     p.r = (unsigned char)r;
     p.g = (unsigned char)g;
@@ -59,16 +65,27 @@ void filter_optimized(void* args[]) {
     Pixel* output = args[3];
     float* filter = args[4];
 
-    for (int x = 0; x < width; ++x) {
-        for (int y = 0; y < height; ++y) {
-            Pixel* p = (Pixel*)malloc(sizeof(Pixel));
-            *p = convolution(input, x, y, width, height, filter);
+    // Use stack memory
+    Pixel p1, p2;
 
-            output[x+y*width].r = p->r;
-            output[x+y*width].g = p->g;
-            output[x+y*width].b = p->b;
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; x += 4) {
+            // Loop unrolling
+            p1 = convolution(input, x, y, width, height, filter);
+            output[x + y * width] = p1;
 
-            free(p);
+            if (x + 1 < width) {
+                p2 = convolution(input, x + 1, y, width, height, filter);
+                output[(x + 1) + y * width] = p2;
+            }
+            if (x + 2 < width) {
+                Pixel p3 = convolution(input, x + 2, y, width, height, filter);
+                output[(x + 2) + y * width] = p3;
+            }
+            if (x + 3 < width) {
+                Pixel p4 = convolution(input, x + 3, y, width, height, filter);
+                output[(x + 3) + y * width] = p4;
+            }
         }
     }
 }
